2c5c864edac62afa4367a3975a2e28aaaa86370e
Shovon Kumar <124199424+shovonkumar@users.noreply.github.com>
2023-06-30 21:10:16 +0600

    All Files Uploaded

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// AOS Activation
+window.addEventListener("load", () => {
+  AOS.init({
+    duration: 1000,
+    easing: "ease-in-out",
+    once: true,
+    mirror: false,
+  });
+});
+
+// MixitUP
+const mixer = mixitup(".myfilter");
+
+// Swiper JS
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
